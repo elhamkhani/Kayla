5198d3fe03cfae8cd86fbb7879385b199dda8625
elham <elham.khani@gmail.com>
2016-02-24 23:57:43 +0000

    add gallery

---
+++ js/gallery.js
@@ -0,0 +1,50 @@
+﻿$(document).ready(function () {
+
+    
+    $(".gallery .cover").hover(function () {
+        if ($(this).hasClass("initialised")) {
+            $(this).attr("style", "background-image:url(img/c" + Math.floor((Math.random() * 9) + 1) + ".png)");
+            $(this).removeClass("initialised");
+        }else
+        {
+            $(this).attr("style", "background-image:url(photos/" + Math.floor((Math.random() * 100) + 1) + ".jpg)");
+            $(this).addClass("initialised");
+        }
+    });
+
+    $(".gallery .cover").click(function () {
+        $(".main-image").attr("style", $(this).attr("style"));
+
+    });
+
+
+
+});
+
+
+$("").fallings({
+
+    velocity: 0,
+    initialPosition: 0,
+    bgParallax: false,
+    bgPercent: "0%",
+    onClass: "fallings-visible",
+    offClass: "fallings-invisible"
+
+});
+
+
+$(function () {
+    $('a[href*="#"]:not([href="#"]), .cover').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
---
+++ js/game.js
@@ -0,0 +1,5 @@
+﻿var trues = ['Once Kayla killed me', '2', '3'];
+var lies = ['4', '5', '6'];
+var all = trues.concat(lies);
+
+document.getElementById("question").innerHTML = all[Math.floor(Math.random() * all.length)];
